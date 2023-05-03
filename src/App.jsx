38cf40c8dc2aa0ef0bb1855d4d0c83d7475b939d
paosua86 <paolasuarezb1986@gmail.com>
2023-05-03 08:18:19 -0500

    hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css'
-import { BrowserRouter as Router, HashRouter, Link, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/navbar';
 import Whatsapp from './components/whatsapp';
 import Home from './components/home';
