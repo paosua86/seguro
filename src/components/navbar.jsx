27c91fe593773d48762d036befbf3faea920825f
paosua86 <paolasuarezb1986@gmail.com>
2023-06-22 10:53:58 -0500

    menu desplegable idiomas

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -159,11 +159,13 @@ function Navbar() {
 
               </li>
               </ul>
+              </div>
               <button data-collapse-toggle="mobile-menu-language-select" type="button" class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-language-select" aria-expanded="false">
                 <span class="sr-only">Open main menu</span>
                 <svg class="w-6 h-6" fill="currentColor" aria-hidden="true" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
               </button>
           </div>
+
           <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="mobile-menu-language-select">
             <a href="/" onClick={(event) => handleNavClick(event, '/')} className="block md:inline-block mr-5 hover:text-gray-900">
               Home
@@ -180,7 +182,7 @@ function Navbar() {
             <a href="https://www.howdengroup.com/" target="_blank" rel="noreferrer" className="block md:inline-block mr-5 hover:text-gray-900">Howden</a>
           </div>
           </div>
-      </div>
+
     </nav>
 
   );
